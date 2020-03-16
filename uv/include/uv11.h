405b8c53bebb6460d2f95d40196a4fc6e7cfe015
wlgq2 <orcaer@yeah.net>
2020-03-17 07:11:24 +0800

    version 1.3.6

--- uv/include/uv11.h
+++ uv/include/uv11.h
@@ -3,7 +3,7 @@
 
    Author: orcaer@yeah.net
 
-   Last modified: 2020-1-10
+   Last modified: 2020-3-17
 
    Description: https://github.com/wlgq2/uv-cpp
 */
@@ -11,7 +11,7 @@
 #ifndef UV_UV11_H
 #define UV_UV11_H
 
-#define    UV_CPP_VERSION     "1.3.5"
+#define    UV_CPP_VERSION     "1.3.6"
 
 #include   "Async.h"
 #include   "Signal.h"
