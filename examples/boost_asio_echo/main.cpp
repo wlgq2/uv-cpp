11d2ebdfd8bd4ea21656edf03ce913b2c05bb825
wlgq2 <orcaer@yeah.net>
2020-04-18 21:48:40 +0800

    echo client

---
+++ examples/boost_asio_echo/main.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+
+
+
+int main(int argc, char** args)
+{
+
+}
---
+++ examples/echo_client/main.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <uv/include/uv11.h>
+
+
+int main(int argc, char** args)
+{
+    uv::EventLoop* loop = uv::EventLoop::DefaultLoop();
+
+    uv::SocketAddr serverAddr("127.0.0.1", 10005, uv::SocketAddr::Ipv4);
+
+    //Tcp Client
+    uv::TcpClient client(loop);
+    client.setConnectStatusCallback(
+        [&client](uv::TcpClient::ConnectStatus status)
+    {
+        if (status == uv::TcpClient::ConnectStatus::OnConnectSuccess)
+        {
+            char data[4096] = "hello world!";
+            client.write(data, sizeof(data));
+        }
+        else
+        {
+            std::cout << "Error : connect to server fail" << std::endl;
+        }
+    });
+    client.setMessageCallback([&client](const char* data,ssize_t size)
+    {
+        client.write(data,size,nullptr);
+    });
+
+    client.connect(serverAddr);
+
+
+    loop->run();
+}
