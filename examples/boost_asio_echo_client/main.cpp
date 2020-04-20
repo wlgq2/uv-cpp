bf1fcc74b13dc278306660b5b35d2ac50730e1c0
wlgq2 <orcaer@yeah.net>
2020-04-20 13:24:26 +0800

    asio example.

--- examples/boost_asio_echo_client/main.cpp
+++ examples/boost_asio_echo_client/main.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <memory>
 #include <atomic>
+#include <vector>
 
 #include <boost/asio.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
@@ -43,11 +44,10 @@ void startRead(SocketPtr ptr)
     pSocket->async_read_some(buffer(buff,size),std::bind(&onRead,ptr,std::placeholders::_1,std::placeholders::_2));
 }
 
-int main(int argc, char* argv[])
+void startClients(io_service& io,std::vector<SocketPtr>& sockets,ip::tcp::endpoint& endpoint, int cnt)
 {
-    io_service io;
-    ip::tcp::endpoint endpoint( ip::address::from_string("127.0.0.1"), 10012);
     SocketPtr ptr = std::make_shared<SocketStr>();
+    sockets.push_back(ptr);
     ptr->pSocket = std::make_shared<ip::tcp::socket>(io);
     ptr->pSocket->async_connect(endpoint, [ptr](const boost::system::error_code& error)
     {
@@ -59,5 +59,13 @@ int main(int argc, char* argv[])
         uint64_t size = 1024*4;
         write(ptr,size);
     });
+}
+
+int main(int argc, char* argv[])
+{
+    io_service io;
+    std::vector<SocketPtr> sockets;
+    ip::tcp::endpoint endpoint( ip::address::from_string("127.0.0.1"), 10012);
+    startClients(io,sockets,endpoint,1);
     io.run();
 }
