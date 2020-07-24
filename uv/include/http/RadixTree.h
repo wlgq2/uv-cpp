49cc96b062bbe87e2f6a9eccdb7bc399ac302b95
wlgq2 <orcaer@yeah.net>
2020-07-24 13:38:00 +0800

    ...

--- uv/include/http/RadixTree.h
+++ uv/include/http/RadixTree.h
@@ -10,13 +10,24 @@
 #ifndef UV_HTTP_RADIX_TREE
 #define UV_HTTP_RADIX_TREE
 
-#include "RadixTreeNode.h"
+#include <memory>
 #include "HttpCommon.h"
 
 namespace uv
 {
 namespace http
 {
+template<typename Type>
+struct RadixTreeNode
+{
+    bool isEmpty;
+    std::string key;
+    std::shared_ptr <RadixTreeNode<Type>> child;
+    std::shared_ptr <RadixTreeNode<Type>> next;
+    Type value;
+};
+template<typename Type>
+using RadixTreeNodePtr = std::shared_ptr<RadixTreeNode<Type>>;
 
 template<typename Type>
 class RadixTree
